81bb5d99fc61eb1c190e9ef134aebd826dc69d22
Wonil Park <parkwonil0522@gmail.com>
2026-05-02 04:11:49 +0900

    [Passed] 일 수 구하기 (8ms, 0MB)

---
+++ Codetree/trail1/일 수 구하기/number-of-days-in-month.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int m;
+    scanf("%d",&m);
+    if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+        printf("%d",31);
+    }else if(m==2){
+        printf("%d",28);
+    }
+    else{
+        printf("%d",30);
+    }
+}
