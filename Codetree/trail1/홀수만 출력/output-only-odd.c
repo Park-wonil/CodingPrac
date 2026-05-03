fd846f0268317985f8d22f52e70288f456023e59
Wonil Park <parkwonil0522@gmail.com>
2026-05-03 14:21:23 +0900

    [Passed] 홀수만 출력 (8ms, 0MB)

---
+++ Codetree/trail1/홀수만 출력/output-only-odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+    for(;a<=b;a++){
+        if(a%2==1){
+            printf("%d ", a);
+        }
+    }
+    return 0;
+}
