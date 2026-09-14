f1b3cbae352372b3908bc7c5e3cbfcd141f1d320
Wonil Park <parkwonil0522@gmail.com>
2026-09-14 16:21:54 +0900

    [Passed] 오름 내림차순 정렬 (2ms, 0MB)

--- Codetree/trail2/오름 내림차순 정렬/inc-dec-sorting.c
+++ Codetree/trail2/오름 내림차순 정렬/inc-dec-sorting.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 
 int n,temp;
-int nums[100];
+int nums[100],nums2[100];
 void sort1(int *nums){
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             if(*(nums+i)>*(nums+j)){
                 temp = *(nums+i);
-                *(nums+i)=*(nums+j);
-                *(nums+j)=temp;
+                *(nums+i) = *(nums+j);
+                *(nums+j) = temp;
             }
         }
     }
     for(int i=0;i<n;i++){
-        printf("%d ",*(nums+i));
+        printf("%d ", nums[i]);
     }
-    printf("\n");
+
 }
-void sort2(int * nums){
-    for(int i=n-1;i>=0;i--){
-        printf("%d ",*(nums+i));
+void sort2(int *nums){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(*(nums+i)<*(nums+j)){
+                temp = *(nums+j);
+                *(nums+j) = *(nums+i);
+                *(nums+i) = temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",nums[i]);
     }
 }
 int main() {
@@ -27,9 +36,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &nums[i]);
     }
-    sort1(nums);
+    sort1(nums); //배열의 첫번쨰 주소를 넘긴다
+    printf("\n");
     sort2(nums);
-    
-    
+
     return 0;
 }
