10135c2b9540f9fac1c05b958cf56c342849707a
Wonil Park <parkwonil0522@gmail.com>
2026-09-14 17:39:48 +0900

    [Passed] 단어 정렬 (2ms, 0MB)

--- Codetree/trail2/단어 정렬/sorting-words.c
+++ Codetree/trail2/단어 정렬/sorting-words.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-char word_list[101][101];
-void sort(char (*word_list)[101],int n){
+char word_list[101][101],temp;
+void sort(char (*word_list)[101],int n){//101개의 행을 가리키는것
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
             if(strcmp(word_list[i],word_list[j])>0){
                 char temp[101];
-                strcpy(temp,word_list[j]);
-                strcpy(word_list[j],word_list[i]);
-                strcpy(word_list[i],temp);
+                strcpy(temp,word_list[i]);
+                strcpy(word_list[i],word_list[j]);
+                strcpy(word_list[j],temp);                               
             }
         }
     }
     for(int i=0;i<n;i++){
-        printf("%s\n",word_list[i]);
+        printf("%s\n", word_list[i]);
     }
-
 }
 int main() {
     int n;
