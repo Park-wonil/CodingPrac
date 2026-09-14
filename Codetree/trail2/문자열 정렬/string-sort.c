30ac5b7a742ae3ae57e3b6091902efa68bbf35b5
Wonil Park <parkwonil0522@gmail.com>
2026-09-14 16:33:34 +0900

    [Passed] 문자열 정렬 (3ms, 0MB)

--- Codetree/trail2/문자열 정렬/string-sort.c
+++ Codetree/trail2/문자열 정렬/string-sort.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
 char str[101],temp;
-void sort1(char *str){
-    for(int i=0;i<strlen(str)-1;i++){
-        for(int j=i+1;j<strlen(str);j++){
+void sort(char *str){ //*str은 첫번째 문자
+    for(int i=0;str[i]!='\0';i++){
+        for(int j=i+1;str[j]!='\0';j++){
             if(str[i]>str[j]){
                 temp = str[i];
-                str[i]=str[j];
-                str[j]=temp; 
+                str[i] = str[j];
+                str[j] = temp;
             }
         }
     }
-    printf("%s",str);
+    printf("%s", str);//전체를 출력하기때문에 전체주소를 보내야함
 }
+
 int main() {
     scanf("%s", str);
-    sort1(str);
+    sort(str);//전체를 출력하기때문에 전체주소를 보내야함
     return 0;
 }
