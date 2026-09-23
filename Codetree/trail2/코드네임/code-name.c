4e7a3a6b7b4fe5c61106ef1273c63ece57acf9c3
Wonil Park <parkwonil0522@gmail.com>
2026-09-23 15:40:54 +0900

    [Passed] 코드네임 (1ms, 0MB)

--- Codetree/trail2/코드네임/code-name.c
+++ Codetree/trail2/코드네임/code-name.c
@@ -9,18 +9,17 @@ User users[5];
 
 int main(){
     int i;
+    User *p = users;
     for(i = 0; i < 5; i++){
-        scanf(" %c %d", &users[i].code_name, &users[i].score);
+        scanf(" %c %d", &(p+i)->code_name, &(p+i)->score);
     }
-    int min = users[0].score,min_idx=0;
-    for(i=1;i<=4;i++){
-        if(min>users[i].score){
-            min = users[i].score;
-            min_idx=i;
+    int min_idx = 0;
+    for(i =1;i<5;i++){
+        if((p+i)->score<(p+min_idx)->score){
+            min_idx = i;
         }
     }
-    printf("%c %d",users[min_idx].code_name,min);
-        
+    printf("%c %d",(p+min_idx)->code_name,(p+min_idx)->score);
 
     
     return 0;
